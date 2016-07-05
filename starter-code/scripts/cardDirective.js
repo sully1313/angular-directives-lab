fb794659bced7e1a68ac773fc00e9cfa74a8e498
Matt Sullivan <mattbsullivan@gmail.com>
2016-07-05 16:36:30 -0700

    ms

---
+++ starter-code/scripts/cardDirective.js
@@ -0,0 +1,11 @@
+angular.module('CardsAgainstAssembly')
+  .directive('wdiCard', wdiCard);
+
+  function wdiCard(){
+    var directive = {
+      restrict: 'E',
+      replace: true,
+      templateUrl:  "templates/cardDirective.html"
+    };
+    return directive;
+  }
